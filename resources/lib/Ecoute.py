6b22d194d5104c3a925f31a58611f6dd863d7e07
sitronet <rondrach@gmail.com>
2020-04-18 18:59:00 +0200

    some change to test

--- resources/lib/Ecoute.py
+++ resources/lib/Ecoute.py
@@ -7,13 +7,13 @@ import threading
 
 import xbmc
 
-TIME_OF_LOOP_SUBSCRIBE = '5' # initial 10 but long time to exit
+TIME_OF_LOOP_SUBSCRIBE = '2' # initial 10 but long time to exit
                              # also delay for the refreshed duration playing
 
-class Souscription(threading.Thread):
-    dataExchangeAbonnement =''
 
-    def __init__(self, InterfaceCli, playerid , Abonnement , recevoirEnAttente , ):
+class Souscription(threading.Thread):
+
+    def __init__(self, InterfaceCli, playerid, Abonnement , recevoirEnAttente ):
         """
 
         :type evenement: threading.Event
@@ -23,6 +23,7 @@ class Souscription(threading.Thread):
         self.playerid = playerid
         self.EtatDuThread = False
         self.recevoirEnAttente = recevoirEnAttente
+
         #self.envoiEnAttente = envoiEnAttente
         #self.LMSCLIport=9090
         #self.LMSCLIip = LMSCLIip
@@ -31,6 +32,7 @@ class Souscription(threading.Thread):
         self.recu = b''
         self.dataExchange = ''
         self.Abonnement = Abonnement
+
 
     def timeofloopsubscribre(self):
         return TIME_OF_LOOP_SUBSCRIBE
@@ -50,9 +52,10 @@ class Souscription(threading.Thread):
         self.InterfaceCLI.sendtoCLISomething(self.playerid + " status 0 200 subscribe:" + TIME_OF_LOOP_SUBSCRIBE)
 
     def resiliersouscription(self):
-        self.InterfaceCLI.sendtoCLISomething(self.playerid + " status - 1 subscribe:- ")
-        poubelle = self.InterfaceCLI.receptionReponseEtDecodage()
-        del poubelle
+        # Todo : define two methode : one with param - , other with 0
+        self.InterfaceCLI.sendtoCLISomething(self.playerid + " status - 2 subscribe:- ")
+        self.InterfaceCLI.receptionReponseEtPoubelle()
+        return
 
     def listen(self):
         xbmc.log("Listen 1 ", xbmc.LOGDEBUG)
@@ -69,19 +72,4 @@ class Souscription(threading.Thread):
         poubelle = self.InterfaceCLI.receptionReponseEtDecodage()
         del poubelle
 
-    def set_volume_player(self, pourcent):
-        self.InterfaceCLI.sendtoCLISomething(self.playerid + ' mixer volume ' +  pourcent )
-        reponse = self.InterfaceCLI.receptionReponseEtDecodage
-        xbmc.log(reponse, xbmc.LOGDEBUG)
-        self.InterfaceCLI.dataExchange = ''
-        self.recevoirEnAttente.clear()
-        if 'volume' in reponse:
-            return True
 
-    def get_volume_player(self):
-        self.InterfaceCLI.sendtoCLISomething(self.playerid + ' mixer volume  ?')
-        reponse = self.InterfaceCLI.receptionReponseEtDecodage
-        xbmc.log(reponse, xbmc.LOGDEBUG)
-        listeB = reponse.split('volume|')
-        volume = float(listeB[1])
-        return volume
